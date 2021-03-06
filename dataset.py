c5f11d88e5d2afb3a8338420e7d4880b546d40d9
ab <iadwivedi@outlook.com>
2021-03-06 18:20:01 +0530

    random loader

--- dataset.py
+++ dataset.py
@@ -4,22 +4,25 @@ import numpy as np
 from itertools import chain, cycle
 from torch.utils.data import IterableDataset
 import torchvision.transforms as transforms
-import librosa, torch
+import librosa, torch, random
 
 
 class TrainDataset(IterableDataset):
-    def __init__(self, wavPath, revPath, samplingRate, segmentLength, nfft, winLength, window):
-        self.sr=samplingRate
-        self.segmentLength=segmentLength
-        self.nfft=nfft
-        self.window=window
-        self.winLength=winLength
+    def __init__(self, wavPath, revPath, samplingRate, segmentLength, nfft, winLength, window, shuffle=True):
+        self.sr = samplingRate
+        self.segmentLength = segmentLength
+        self.nfft = nfft
+        self.window = window
+        self.winLength = winLength
         self.wavPath = wavPath
         self.revPath = revPath
         self.ids = [i for i in listdir(revPath) if not i.startswith('.')]
         self.transform = transforms.Compose([
             transforms.Normalize((0.5,), (0.5,))
         ])
+
+        if shuffle:
+            random.shuffle(self.ids)
 
     def __len__(self):
         return len(self.ids)
@@ -34,8 +37,10 @@ class TrainDataset(IterableDataset):
         rev = glob(self.revPath + idx)[0]
         org, _ = librosa.load(org, sr=self.sr)
         rev, _ = librosa.load(rev, sr=self.sr)
-        org = np.abs(librosa.stft(org, n_fft=self.nfft, window=self.window, win_length=self.winLength))[1:, :self.segmentLength]
-        rev = np.abs(librosa.stft(rev, n_fft=self.nfft, window=self.window, win_length=self.winLength))[1:, :self.segmentLength]
+        org = np.abs(librosa.stft(org, n_fft=self.nfft, window=self.window, win_length=self.winLength))[1:,
+              :self.segmentLength]
+        rev = np.abs(librosa.stft(rev, n_fft=self.nfft, window=self.window, win_length=self.winLength))[1:,
+              :self.segmentLength]
         orgArray = torch.FloatTensor(list(self.squaredChunks(np.abs(org.T))))
         revArray = torch.FloatTensor(list(self.squaredChunks(np.abs(rev.T))))
         for i, v in enumerate(revArray):
@@ -46,4 +51,3 @@ class TrainDataset(IterableDataset):
 
     def __iter__(self):
         return self.getStream(self.ids)
-
